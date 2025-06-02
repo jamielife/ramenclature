786f296298b547872aa857da917f8f2bdc3137c7
Jamie Taylor <jvptaylor@hotmail.com>
2025-06-02 15:25:37 -0400

    Initial build

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import node from '@astrojs/node';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap()],
+
+    vite: {
+      plugins: [tailwindcss()],
+    },
+
+    adapter: node({
+      mode: 'standalone',
+    }),
+
+    image: {
+      domains: ["ramen-api.dev"],
+    },  
+});
---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Rāmen·clature';
+export const SITE_DESCRIPTION = 'Define your next favorite bowl of ramen!';
+
+export const API_URL = 'https://ramen-api.dev/shops?pretty&page=3&perPage=3';
